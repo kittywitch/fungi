7973f8410ec3d0bad3b794d47f3ae2f66696f6df
Kat Inskip <kat@inskip.me>
2026-02-12 08:39:30 -0800

    fix: oops, space

--- src/kusachi/components.clj
+++ src/kusachi/components.clj
@@ -210,7 +210,7 @@
       [:h3 {:id "contact"} "Contact"]
       [:nav
         [:ul
-          [:li "kat on" (link "liberachat" "https://libera.chat/")]
+          [:li "kat on " (link "liberachat" "https://libera.chat/")]
           [:li "@kat:kittywit.ch on Matrix"]
           [:li "kat (at) kittywit.ch on XMPP, OMEMO supported"]
           [:li "this domain has a catch-all for email, note to self, provide PGP"]
